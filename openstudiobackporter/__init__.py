a63db9bf97f5c8198f719c24828c173872758c90
Julien Marrec <julien.marrec@gmail.com>
2025-07-10 17:59:54 +0200

    minor update

---
+++ openstudiobackporter/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for openstudio-backporter."""
+
+__author__ = """Julien Marrec"""
+__email__ = 'contact@effibem.com'
+__version__ = '3.10.0'
+
+from openstudiobackporter.backporter import Backporter
+
+__all__ = ["Backporter"]
---
+++ openstudiobackporter/backporter.py
@@ -0,0 +1,2 @@
+class Backporter:
+    pass
