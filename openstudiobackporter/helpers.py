bcdc398a28e9f8c9c444de14c9e37a259b408324
Julien Marrec <julien.marrec@gmail.com>
2025-12-10 11:05:54 +0100

    Nasty subtle things: m_handle isn't set when you deal with IdfFile and call setString(0)...

--- openstudiobackporter/helpers.py
+++ openstudiobackporter/helpers.py
@@ -22,13 +22,25 @@ def get_target(idf_file: openstudio.IdfFile, idf_obj: openstudio.IdfObject, inde
         print(f"For {brief_description(idf_obj=idf_obj)}, String at index {index} is not initialized.")
         return openstudio.OptionalIdfObject()
 
-    return idf_file.getObject(openstudio.toUUID(handle_.get()))
+    handle_str = handle_.get()
+
+    # m_handle isn't being set properly until you reload the model, so we have to look it up ourselves
+    for x in idf_file.objects():
+        if x.getString(0).value_or('') == handle_str:
+            return openstudio.OptionalIdfObject(x)
+
+    return openstudio.OptionalIdfObject()
 
 
 def copy_object_as_is(obj: openstudio.IdfObject, newObject: openstudio.IdfObject) -> None:
     """Copy an IdfObject as is.
 
     Even though the object didn't change, the IddObject might have changed field names or memo, etc.
+
+    Args:
+    -----
+    * obj: (openstudio.IdfObject) (float): The source IdfObject, from the newer version
+    * newObject: (openstudio.IdfObject) The target IdfObject, from the older version
     """
     for i in range(obj.numFields()):
         if value := obj.getString(i):
@@ -38,7 +50,15 @@ def copy_object_as_is(obj: openstudio.IdfObject, newObject: openstudio.IdfObject
 def copy_with_deleted_fields(
     obj: openstudio.IdfObject, newObject: openstudio.IdfObject, skip_indices: set[int]
 ) -> None:
-    """Copy an IdfObject while skipping certain field indices."""
+    """Copy an IdfObject while skipping certain field indices.
+
+    Args:
+    -----
+    * obj: (openstudio.IdfObject) (float): The source IdfObject, from the newer version
+    * newObject: (openstudio.IdfObject) The target IdfObject, from the older version
+    * skip_indices: (set[int]) The set of field indices to skip (0-indexed)
+    """
+
     offset = 0
     for i in range(obj.numFields()):
         if i in skip_indices:
@@ -49,7 +69,14 @@ def copy_with_deleted_fields(
 
 
 def copy_with_cutoff_fields(obj: openstudio.IdfObject, newObject: openstudio.IdfObject, cutoff_index: int) -> None:
-    """Copy an IdfObject while skipping fields from a certain index onward."""
+    """Copy an IdfObject while skipping fields from a certain index onward.
+
+    Args:
+    -----
+    * obj: (openstudio.IdfObject) (float): The source IdfObject, from the newer version
+    * newObject: (openstudio.IdfObject) The target IdfObject, from the older version
+    * cutoff_index: (int) The index from which to stop copying fields (0-indexed)
+    """
     for i in range(obj.numFields()):
         if i >= cutoff_index:
             break
